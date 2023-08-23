0a0e71b0b6555b16904d339f7081783a3772ce38
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-23 23:28:08 +0530

    init

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <>
+    <div className=' w-full flex h-[104px] px-[3%] items-center bg-[#fff]'>
+        <nav className=' max-w-[1340px] w-full mx-auto flex items-center justify-between'>
+
+        </nav>
+    </div>
+   </>
+  )
+}
+
+export default Header
