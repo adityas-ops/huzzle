771689785cd6847fd3e3c856d358b0fedb0c125b
adityas-opse <adityakushinagar123@gmail.com>
2023-08-27 01:12:49 +0530

    complete section 4

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 import Section1 from '@/components/Section1'
 import Section2 from '@/components/Section2'
 import Section3 from '@/components/Section3'
+import Section4 from '@/components/Section4'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function Home() {
     <Section1/>
     <Section2/>
     <Section3/>
+    <Section4/>
    </>
   )
 }
